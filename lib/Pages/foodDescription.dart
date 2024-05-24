49ea0f30be9c70606cd9f7ed77cc0e50e36b36d4
JohnGastone <johngastone11@gmail.com>
2024-05-24 22:37:57 +0300

    Fudi description+++

--- lib/Pages/foodDescription.dart
+++ lib/Pages/foodDescription.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +7,27 @@ class FudiDescription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 221, 206, 206),
+        title: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.arrow_back_ios_new),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
