347456ed52d423fc6d20361a48461a42871069ec
JohnGastone <johngastone11@gmail.com>
2024-05-25 23:47:03 +0300

    +++

--- lib/Pages/cartPage.dart
+++ lib/Pages/cartPage.dart
@@ -118,6 +118,228 @@ class _CartPageState extends State<CartPage> {
                   ),
                 ),
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Container(
+                width: 350,
+                height: 130,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white54),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "./assets/bugger.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Chicken Bugger",
+                              style: GoogleFonts.spaceMono(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "\$ 9.5",
+                              style: GoogleFonts.spaceMono(
+                                  fontSize: 18, color: Colors.green),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SizedBox(
+                        height: 140,
+                        width: 30,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 215, 211, 211),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            children: [
+                              Text(
+                                "+",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              ),
+                              Text(
+                                "1",
+                                style: GoogleFonts.spaceMono(
+                                    fontSize: 20, color: Colors.green),
+                              ),
+                              Text(
+                                "-",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Container(
+                width: 350,
+                height: 130,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white54),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "./assets/bugger.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Chicken Bugger",
+                              style: GoogleFonts.spaceMono(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "\$ 9.5",
+                              style: GoogleFonts.spaceMono(
+                                  fontSize: 18, color: Colors.green),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SizedBox(
+                        height: 140,
+                        width: 30,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 215, 211, 211),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            children: [
+                              Text(
+                                "+",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              ),
+                              Text(
+                                "1",
+                                style: GoogleFonts.spaceMono(
+                                    fontSize: 20, color: Colors.green),
+                              ),
+                              Text(
+                                "-",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Container(
+                width: 350,
+                height: 130,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white54),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "./assets/hamburger.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Beef Bugger",
+                              style: GoogleFonts.spaceMono(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "\$ 12.5",
+                              style: GoogleFonts.spaceMono(
+                                  fontSize: 18, color: Colors.green),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SizedBox(
+                        height: 140,
+                        width: 30,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 215, 211, 211),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            children: [
+                              Text(
+                                "+",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              ),
+                              Text(
+                                "1",
+                                style: GoogleFonts.spaceMono(
+                                    fontSize: 20, color: Colors.green),
+                              ),
+                              Text(
+                                "-",
+                                style: GoogleFonts.spaceMono(fontSize: 30),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             )
           ],
         ),
