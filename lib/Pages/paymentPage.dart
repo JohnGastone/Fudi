a5fc928a0a944e36a4fbb8353b2760f7b3633bc5
JohnGastone <johngastone11@gmail.com>
2024-05-26 10:04:25 +0300

    +++

---
+++ lib/Pages/paymentPage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types
+
+import "package:flutter/material.dart";
+
+class paymentPage extends StatefulWidget {
+  const paymentPage({super.key});
+
+  @override
+  State<paymentPage> createState() => _paymentPageState();
+}
+
+class _paymentPageState extends State<paymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 221, 206, 206),
+    );
+  }
+}
