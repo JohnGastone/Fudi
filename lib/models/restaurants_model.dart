3f198d6fcf0729c665293dccd119d568d28c3171
JohnGastone <johngastone11@gmail.com>
2024-05-27 21:20:10 +0300

    Restaurants+++

---
+++ lib/models/restaurants_model.dart
@@ -0,0 +1,7 @@
+class RestaurantModel {
+  String? restaurantName;
+  double? restaurantRating;
+  String? coverImage;
+
+  RestaurantModel(this.restaurantName, this.restaurantRating, this.coverImage);
+}
