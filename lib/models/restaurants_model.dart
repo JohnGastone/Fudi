3686edf7a838e8f80a4db7dcdb67b00335501c4b
JohnGastone <johngastone11@gmail.com>
2024-05-27 22:39:13 +0300

    Restaurants+++

--- lib/models/restaurants_model.dart
+++ lib/models/restaurants_model.dart
@@ -10,6 +10,12 @@ class RestaurantList {
   static List<RestaurantModel> getRestaurants = [
     RestaurantModel("Seafood Lovers", 5.0, "./assets/hamburger.png"),
     RestaurantModel("Shishi Food   ", 5.0, "./assets/biryani.png"),
+    RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png"),
+    RestaurantModel("Seafood Lovers", 5.0, "./assets/hamburger.png"),
+    RestaurantModel("Shishi Food   ", 5.0, "./assets/biryani.png"),
+    RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png"),
+    RestaurantModel("Seafood Lovers", 5.0, "./assets/hamburger.png"),
+    RestaurantModel("Shishi Food   ", 5.0, "./assets/biryani.png"),
     RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png")
   ];
   static List<RestaurantModel> displayList = List.from(getRestaurants);
