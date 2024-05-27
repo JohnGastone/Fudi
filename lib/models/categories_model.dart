62b4cd1ea4d912d5ef12be3196493de52e28bdd3
JohnGastone <johngastone11@gmail.com>
2024-05-27 22:46:53 +0300

    Restaurants+++

--- lib/models/categories_model.dart
+++ lib/models/categories_model.dart
@@ -4,3 +4,19 @@ class FoodCategories {
 
   FoodCategories({this.categoryName, this.categoryImage});
 }
+
+class CategoriesList {
+  static List<FoodCategories>? foodCategories = [
+    FoodCategories(
+        categoryName: "Fruits", categoryImage: "assets/images/fruits.png"),
+    FoodCategories(
+        categoryName: "Vegetables",
+        categoryImage: "assets/images/vegetables.png"),
+    FoodCategories(
+        categoryName: "Meat", categoryImage: "assets/images/meat.png"),
+    FoodCategories(
+        categoryName: "Dairy", categoryImage: "assets/images/dairy.png"),
+    FoodCategories(
+        categoryName: "Others", categoryImage: "assets/images/others.png"),
+  ];
+}
