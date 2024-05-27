dd0e2c11f438b3ad6475c3e4b6eb79fed554be04
JohnGastone <johngastone11@gmail.com>
2024-05-27 21:26:12 +0300

    Restaurants+++

--- lib/models/restaurants_model.dart
+++ lib/models/restaurants_model.dart
@@ -5,3 +5,11 @@ class RestaurantModel {
 
   RestaurantModel(this.restaurantName, this.restaurantRating, this.coverImage);
 }
+
+class RestaurantList {
+  static List<RestaurantModel> getRestaurants = [
+    RestaurantModel("Seafood Lovers", 5.0, "./assets/SeafoodLovers.png"),
+    RestaurantModel("Shishi", 5.0, "./assets/shishifood.png"),
+    RestaurantModel("Shangazi Cafe", 4.5, "./assets/shangazi.png")
+  ];
+}
