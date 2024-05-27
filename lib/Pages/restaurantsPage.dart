cc79bbb5f82fca22f9974215d3a60be57a1a99fa
JohnGastone <johngastone11@gmail.com>
2024-05-27 19:23:24 +0300

    Restaurants+++

---
+++ lib/Pages/restaurantsPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RestaurantsPage extends StatelessWidget {
+  const RestaurantsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
