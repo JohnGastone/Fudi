22a597eaff085441ee0824c85bdfeeefb1116f2d
JohnGastone <johngastone11@gmail.com>
2024-05-28 13:33:15 +0300

    Popular foods++

---
+++ lib/models/popularFoods_model.dart
@@ -0,0 +1,8 @@
+class PopularFoodsModel {
+  String? foodCoverImage;
+  String? foodName;
+  String? foodTag;
+  String? foodPrice;
+  PopularFoodsModel(
+      this.foodCoverImage, this.foodName, this.foodTag, this.foodPrice);
+}
