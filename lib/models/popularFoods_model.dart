b73f3f894638b62da725aab9ba48fe2545113794
JohnGastone <johngastone11@gmail.com>
2024-05-28 15:13:51 +0300

    Popular foods++

--- lib/models/popularFoods_model.dart
+++ lib/models/popularFoods_model.dart
@@ -9,12 +9,12 @@ class PopularFoodsModel {
 
 class PopularFoodsList {
   static List<PopularFoodsModel> getPopularFoods = [
-    PopularFoodsModel("./assets/hamburger.png", "Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/bugger.png", "Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/pizza.png", "Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/pilau.png", "Pizza", "Pizza", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Harmbugger", "Pizza", 15),
+    PopularFoodsModel("./assets/bugger.png", "Latino Bugger", "Pizza", 15),
+    PopularFoodsModel("./assets/pizza.png", "Santorini Pizza", "Pizza", 15),
+    PopularFoodsModel("./assets/pilau.png", "Pilau Vuruga", "Pizza", 15),
     PopularFoodsModel("./assets/biryani.png", "Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/mzuzu.png", "Pizza", "Pizza", 15),
+    PopularFoodsModel("./assets/mzuzu.png", "Wali Mzuzu", "Pizza", 15),
     PopularFoodsModel("./assets/margeritha.png", "Pizza", "Pizza", 15),
     PopularFoodsModel("./assets/hamburger.png", "Pizza", "Pizza", 15),
     PopularFoodsModel("./assets/hamburger.png", "Pizza", "Pizza", 15),
