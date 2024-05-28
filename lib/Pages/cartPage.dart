5f69032ba2ae40b82fb727fa5c9f307842f7f100
JohnGastone <johngastone11@gmail.com>
2024-05-28 20:04:05 +0300

    +++

--- lib/Pages/cartPage.dart
+++ lib/Pages/cartPage.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:fudi/Pages/homepage.dart';
+import 'package:fudi/Pages/explorePage.dart';
 import 'package:fudi/Pages/paymentPage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
