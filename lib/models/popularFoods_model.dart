cef9e256478b85099eef6e8b3d8f96151df9990e
JohnGastone <johngastone11@gmail.com>
2024-05-29 11:06:57 +0300

    Restaurants+++

--- lib/models/popularFoods_model.dart
+++ lib/models/popularFoods_model.dart
@@ -9,21 +9,24 @@ class PopularFoodsModel {
 
 class PopularFoodsList {
   static List<PopularFoodsModel> getPopularFoods = [
-    PopularFoodsModel("./assets/hamburger.png", "Harmbugger", "Pizza", 15),
-    PopularFoodsModel("./assets/bugger.png", "Latino Bugger", "Pizza", 15),
-    PopularFoodsModel("./assets/pizza.png", "Santorini Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/pilau.png", "Pilau Vuruga", "Pizza", 15),
-    PopularFoodsModel("./assets/biryani.png", "Pizza", "Pizza", 15),
-    PopularFoodsModel("./assets/mzuzu.png", "Wali Mzuzu", "Pizza", 15),
-    PopularFoodsModel("./assets/margeritha.png", "Nyama Choma", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Chips Mayai", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Wali Nazi", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Zanzibar Pilau", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Matoke", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Makande", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Makange Nyama", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Makange Samaki", "Pizza", 15),
-    PopularFoodsModel("./assets/hamburger.png", "Ugali", "Pizza", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Harmbugger", "Sweet", 15),
+    PopularFoodsModel("./assets/bugger.png", "Latino Bugger", "Succulent", 15),
+    PopularFoodsModel("./assets/pizza.png", "Santorini Pizza", "Flavorful", 15),
+    PopularFoodsModel("./assets/pilau.png", "Pilau Vuruga", "Aromatic", 15),
+    PopularFoodsModel("./assets/biryani.png", "Pizza", "Fluffy", 15),
+    PopularFoodsModel("./assets/mzuzu.png", "Wali Mzuzu", "Sweet", 15),
+    PopularFoodsModel("./assets/margeritha.png", "Nyama Choma", "Spicy", 15),
+    PopularFoodsModel(
+        "./assets/hamburger.png", "Chips Mayai", "Comforting", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Wali Nazi", "Creamy", 15),
+    PopularFoodsModel(
+        "./assets/hamburger.png", "Zanzibar Pilau", "Aromatic", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Matoke", "Savory", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Makande", "Nutritious", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Makange Nyama", "Hearty", 15),
+    PopularFoodsModel(
+        "./assets/hamburger.png", "Makange Samaki", "Flavorful", 15),
+    PopularFoodsModel("./assets/hamburger.png", "Ugali", "Satisfying", 15),
   ];
   static List<PopularFoodsModel> displayPopularFoods =
       List.from(getPopularFoods);
