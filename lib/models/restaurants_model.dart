53a040a8605b3960697b78a359ef605fa5f13f89
JohnGastone <johngastone11@gmail.com>
2024-05-29 08:05:36 +0300

    +++

--- lib/models/restaurants_model.dart
+++ lib/models/restaurants_model.dart
@@ -2,21 +2,28 @@ class RestaurantModel {
   String? restaurantName;
   double? restaurantRating;
   String? coverImage;
+  String? restaurantLocation;
 
-  RestaurantModel(this.restaurantName, this.restaurantRating, this.coverImage);
+  RestaurantModel(this.restaurantName, this.restaurantRating, this.coverImage,
+      this.restaurantLocation);
 }
 
 class RestaurantList {
   static List<RestaurantModel> getRestaurants = [
-    RestaurantModel("Seafood Lovers", 5.0, "./assets/hamburger.png"),
-    RestaurantModel("Shishi Food   ", 5.0, "./assets/biryani.png"),
-    RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png"),
-    RestaurantModel("External Cafe", 5.0, "./assets/hamburger.png"),
-    RestaurantModel("Mautamu Spices   ", 5.0, "./assets/biryani.png"),
-    RestaurantModel("Kazimoto Porks ", 4.5, "./assets/pilau.png"),
-    RestaurantModel("Mr Liverpool", 5.0, "./assets/hamburger.png"),
-    RestaurantModel("Shishi Food   ", 5.0, "./assets/biryani.png"),
-    RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png")
+    RestaurantModel(
+        "Seafood Lovers", 5.0, "./assets/hamburger.png", "Kinondoni B"),
+    RestaurantModel(
+        "Shishi Food   ", 5.0, "./assets/biryani.png", "Kijitonyama"),
+    RestaurantModel(
+        "Shangazi Cafe ", 4.5, "./assets/pilau.png", "Dumila Chini"),
+    RestaurantModel("External Cafe", 5.0, "./assets/hamburger.png", "External"),
+    RestaurantModel(
+        "Mautamu Spices   ", 5.0, "./assets/biryani.png", "Mbezi Mwisho"),
+    RestaurantModel("Kazimoto Porks ", 4.5, "./assets/pilau.png", "Segerea"),
+    RestaurantModel(
+        "Mr Liverpool", 5.0, "./assets/hamburger.png", "Mpanda Mjini"),
+    RestaurantModel("Sossie Foods   ", 5.0, "./assets/biryani.png", ""),
+    RestaurantModel("Shangazi Cafe ", 4.5, "./assets/pilau.png", "")
   ];
   static List<RestaurantModel> displayList = List.from(getRestaurants);
 }
