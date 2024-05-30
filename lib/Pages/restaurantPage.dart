c8c111ee9a41cbe01ca04964b5f78dec994b8aca
JohnGastone <johngastone11@gmail.com>
2024-05-30 11:14:29 +0300

    Restaurant+++

--- lib/Pages/restaurantPage.dart
+++ lib/Pages/restaurantPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 class RestaurantPage extends StatefulWidget {
   const RestaurantPage({super.key});
