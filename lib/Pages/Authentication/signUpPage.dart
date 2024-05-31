831bc151d707b58e73a18ed4598ce0337ea69968
JohnGastone <johngastone11@gmail.com>
2024-06-01 00:23:04 +0300

    +++

--- lib/Pages/Authentication/signUpPage.dart
+++ lib/Pages/Authentication/signUpPage.dart
@@ -1,10 +1,162 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../explorePage.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 36, 48, 79),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromARGB(255, 36, 48, 79),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: Text(
+                'Welcome Back!',
+                style: GoogleFonts.spaceMono(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, right: 20),
+              child: Text(
+                "Please enter your credentials below, there is a dish waiting for you",
+                style: GoogleFonts.spaceMono(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 221, 206, 206),
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30))),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 270,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(26),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Enter your username i.e. Msambi Nungwa",
+                            hintStyle: GoogleFonts.spaceMono(fontSize: 15),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 270,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(26),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: "Enter your email address",
+                            hintStyle: GoogleFonts.spaceMono(fontSize: 15),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 270,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(26),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            hintText: "Enter your phone number",
+                            hintStyle: GoogleFonts.spaceMono(fontSize: 15),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 270,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(26),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Enter your password",
+                            hintStyle: GoogleFonts.spaceMono(fontSize: 15),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(26)),
+                      height: 50,
+                      width: 270,
+                      child: FloatingActionButton(
+                          child: Text(
+                            "Login",
+                            style: GoogleFonts.spaceMono(fontSize: 18),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MyHomePage()));
+                          }))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
