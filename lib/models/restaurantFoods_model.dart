1388f791c5958dcb80e57280dd4398bfd7a0aa3a
JohnGastone <johngastone11@gmail.com>
2024-05-31 11:52:36 +0300

    Restaurant+++

---
+++ lib/models/restaurantFoods_model.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:fudi/models/restaurants_model.dart';
+
+class RestaurantfoodsModel {
+  String? foodName;
+  int? foodPrice;
+  String? preparationTime;
+  String? foodImage;
+
+  RestaurantfoodsModel(
+      this.foodName, this.foodPrice, this.foodImage, this.preparationTime);
+}
+
+class RestaurantFoodlist {
+  static List<RestaurantModel> getRestaurantFood = [];
+}
