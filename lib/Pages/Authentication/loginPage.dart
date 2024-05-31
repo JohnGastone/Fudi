8a8d83c7e0645f07e612e4d9de37b5446a63c583
JohnGastone <johngastone11@gmail.com>
2024-05-31 23:25:43 +0300

    +++

--- lib/Pages/Authentication/loginPage.dart
+++ lib/Pages/Authentication/loginPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
@@ -13,12 +14,30 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 36, 48, 79),
       appBar: AppBar(
         automaticallyImplyLeading: false,
+        backgroundColor: Color.fromARGB(255, 36, 48, 79),
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: Text(
+                'Welcome Back',
+                style: GoogleFonts.spaceMono(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
