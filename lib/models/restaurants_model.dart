753086d9d5a5da44bdde427049356cb14be18313
JohnGastone <johngastone11@gmail.com>
2024-05-31 13:08:26 +0300

    Restaurant+++

--- lib/models/restaurants_model.dart
+++ lib/models/restaurants_model.dart
@@ -16,20 +16,20 @@ class RestaurantList {
     RestaurantModel(
         "Shishi Food   ", 5.0, "./assets/shishi.png", "Kijitonyama", 2),
     RestaurantModel(
-        "Shangazi Cafe ", 4.5, "./assets/pilau.png", "Dumila Chini", 3),
+        "Shangazi Cafe ", 4.5, "./assets/Seafood.png", "Dumila Chini", 3),
+    RestaurantModel("External Cafe", 5.0, "./assets/shishi.png", "External", 4),
     RestaurantModel(
-        "External Cafe", 5.0, "./assets/hamburger.png", "External", 4),
+        "Mautamu Spices   ", 5.0, "./assets/Seafood.png", "Mbezi Mwisho", 5),
     RestaurantModel(
-        "Mautamu Spices   ", 5.0, "./assets/biryani.png", "Mbezi Mwisho", 5),
-    RestaurantModel("Kazimoto Porks ", 4.5, "./assets/pilau.png", "Segerea", 6),
+        "Kazimoto Porks ", 4.5, "./assets/shishi.png", "Segerea", 6),
     RestaurantModel(
-        "Mr Liverpool", 5.0, "./assets/hamburger.png", "Mpanda Mjini", 7),
+        "Mr Liverpool", 5.0, "./assets/Seafood.png", "Mpanda Mjini", 7),
     RestaurantModel(
-        "Sossie Foods   ", 5.0, "./assets/biryani.png", "Kinyerezi", 8),
+        "Sossie Foods   ", 5.0, "./assets/shishi.png", "Kinyerezi", 8),
     RestaurantModel(
-        "Shangazi Chichi ", 4.5, "./assets/pilau.png", "Msasani", 9),
+        "Shangazi Chichi ", 4.5, "./assets/Seafood.png", "Msasani", 9),
     RestaurantModel(
-        "Shangazi Chichi ", 4.5, "./assets/pilau.png", "Msasani", 10)
+        "Shangazi Chichi ", 4.5, "./assets/shishi.png", "Msasani", 10)
   ];
   static List<RestaurantModel> displayList = List.from(getRestaurants);
 }
