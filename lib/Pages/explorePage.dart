fa81e0d3be1a4122fbb096999df99018fa16926b
JohnGastone <johngastone11@gmail.com>
2024-06-01 19:34:40 +0300

    +++

--- lib/Pages/explorePage.dart
+++ lib/Pages/explorePage.dart
@@ -393,7 +393,7 @@ class _ExplorePageState extends State<ExplorePage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: SizedBox(
-                height: 300,
+                // height: 300,
                 child: GridView.builder(
                   physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
