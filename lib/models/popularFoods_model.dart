a43f2fb50adbc0c91eef4088627e1d49db685596
JohnGastone <johngastone11@gmail.com>
2024-06-02 10:28:56 +0300

    +++

--- lib/models/popularFoods_model.dart
+++ lib/models/popularFoods_model.dart
@@ -12,23 +12,23 @@ class PopularFoodsModel {
 class PopularFoodsList {
   static List<PopularFoodsModel> getPopularFoods = [
     PopularFoodsModel("./assets/hamburger.png", "Harmbugger", "Sweet", 15),
-    PopularFoodsModel("./assets/bugger.png", "Latino Bugger", "Succulent", 15),
-    PopularFoodsModel("./assets/pizza.png", "Santorini Pizza", "Flavorful", 15),
-    PopularFoodsModel("./assets/pilau.png", "Pilau Vuruga", "Aromatic", 15),
-    PopularFoodsModel("./assets/biryani.png", "Pizza", "Fluffy", 15),
-    PopularFoodsModel("./assets/mzuzu.png", "Wali Mzuzu", "Sweet", 15),
+    PopularFoodsModel("./assets/bugger.png", "Latino Bugger", "Succulent", 9),
+    PopularFoodsModel("./assets/pizza.png", "Santorini Pizza", "Flavorful", 11),
+    PopularFoodsModel("./assets/pilau.png", "Pilau Vuruga", "Aromatic", 10),
+    PopularFoodsModel("./assets/biryani.png", "Pizza", "Fluffy", 7),
+    PopularFoodsModel("./assets/mzuzu.png", "Wali Mzuzu", "Sweet", 14),
     PopularFoodsModel("./assets/margeritha.png", "Nyama Choma", "Spicy", 15),
-    PopularFoodsModel("./assets/chips.png", "Chips Mayai", "Sweet", 15),
-    PopularFoodsModel("./assets/nazi.png", "Wali Nazi", "Creamy", 15),
+    PopularFoodsModel("./assets/chips.png", "Chips Mayai", "Sweet", 5),
+    PopularFoodsModel("./assets/nazi.png", "Wali Nazi", "Creamy", 12),
     PopularFoodsModel(
         "./assets/pilauzanzibar.png", "Zanzibar Pilau", "Aromatic", 15),
-    PopularFoodsModel("./assets/matoke.png", "Matoke", "Savory", 15),
-    PopularFoodsModel("./assets/makande.png", "Makande", "Nutritious", 15),
+    PopularFoodsModel("./assets/matoke.png", "Matoke", "Savory", 10),
+    PopularFoodsModel("./assets/makande.png", "Makande", "Nutritious", 4),
     PopularFoodsModel(
-        "./assets/makangenyama.png", "Makange Nyama", "Hearty", 15),
+        "./assets/makangenyama.png", "Makange Nyama", "Hearty", 13),
     PopularFoodsModel(
         "./assets/makangesamaki.png", "Makange Samaki", "Flavorful", 15),
-    PopularFoodsModel("./assets/ugali.png", "Ugali", "Satisfying", 15),
+    PopularFoodsModel("./assets/ugali.png", "Ugali", "Satisfying", 11),
   ];
   static List<PopularFoodsModel> displayPopularFoods =
       List.from(getPopularFoods);
