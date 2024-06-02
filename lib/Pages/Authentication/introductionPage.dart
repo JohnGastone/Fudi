8a37a9f675c1c9b66c3709c2e6cb21e16046f6d7
JohnGastone <johngastone11@gmail.com>
2024-06-02 10:58:46 +0300

    +++

--- lib/Pages/Authentication/introductionPage.dart
+++ lib/Pages/Authentication/introductionPage.dart
@@ -18,103 +18,55 @@ class _IntroductionPageState extends State<IntroductionPage> {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 87, 145, 21),
       body: SingleChildScrollView(
-        child: Flexible(
-          child: Center(
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    height: 150,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.only(bottomRight: Radius.circular(100)),
-                      color: Colors.white24,
-                    ),
+        child: Center(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 150,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(100)),
+                    color: Colors.white24,
                   ),
                 ),
-                SizedBox(
-                  height: 100,
-                ),
-                Text(
-                  "Hello.",
-                  style: GoogleFonts.spaceMono(
-                      fontSize: 60,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Let's get you a dish",
-                        style: GoogleFonts.spaceMono(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      Image.asset(
-                        "./assets/blink.png",
-                        height: 40,
-                        width: 40,
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                    child: Container(
-                      height: 50,
-                      width: 140,
-                      decoration: BoxDecoration(
-                          //boxShadow: ,
-                          borderRadius: BorderRadius.circular(30),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "Hello.",
+                style: GoogleFonts.spaceMono(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      "Let's get you a dish",
+                      style: GoogleFonts.spaceMono(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
                           color: Colors.white),
-                      child: Center(
-                        child: Text(
-                          "Login",
-                          style: GoogleFonts.spaceMono(fontSize: 30),
-                        ),
-                      ),
                     ),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Loginpage()));
-                    }),
-                SizedBox(
-                  height: 20,
+                    Image.asset(
+                      "./assets/blink.png",
+                      height: 40,
+                      width: 40,
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 50, right: 20),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                          width: 100,
-                          child: Divider(
-                            color: Colors.white,
-                          )),
-                      Text(
-                        "  Or  ",
-                        style: GoogleFonts.spaceMono(
-                            fontSize: 20, color: Colors.white),
-                      ),
-                      SizedBox(
-                          width: 100,
-                          child: Divider(
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                InkWell(
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
                   child: Container(
                     height: 50,
                     width: 140,
@@ -124,33 +76,79 @@ class _IntroductionPageState extends State<IntroductionPage> {
                         color: Colors.white),
                     child: Center(
                       child: Text(
-                        "Sign Up",
+                        "Login",
                         style: GoogleFonts.spaceMono(fontSize: 30),
                       ),
                     ),
                   ),
                   onTap: () {
                     Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()));
-                  },
+                        MaterialPageRoute(builder: (context) => Loginpage()));
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 20),
+                child: Row(
+                  children: [
+                    SizedBox(
+                        width: 100,
+                        child: Divider(
+                          color: Colors.white,
+                        )),
+                    Text(
+                      "  Or  ",
+                      style: GoogleFonts.spaceMono(
+                          fontSize: 20, color: Colors.white),
+                    ),
+                    SizedBox(
+                        width: 100,
+                        child: Divider(
+                          color: Colors.white,
+                        )),
+                  ],
                 ),
-                SizedBox(
-                  height: 80,
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    height: 150,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(100)),
-                      color: Colors.white24,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                child: Container(
+                  height: 50,
+                  width: 140,
+                  decoration: BoxDecoration(
+                      //boxShadow: ,
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: GoogleFonts.spaceMono(fontSize: 30),
                     ),
                   ),
                 ),
-              ],
-            ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()));
+                },
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  height: 150,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(100)),
+                    color: Colors.white24,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
