93bd1be3f4de74eb6dcc3a7613d63802f9e49693
JohnGastone <johngastone11@gmail.com>
2024-06-03 19:20:40 +0300

    ++++

---
+++ lib/models/orders_model.dart
@@ -0,0 +1,11 @@
+class FoodOrdersModel {
+  DateTime? orderDate;
+  String? orderID;
+  String? orderStatus;
+
+  FoodOrdersModel({
+    this.orderDate,
+    this.orderID,
+    this.orderStatus,
+  });
+}
