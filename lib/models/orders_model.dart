f6498f5a3fc7a1bc79220e6b134c15fbbb5acf31
JohnGastone <johngastone11@gmail.com>
2024-06-03 19:30:36 +0300

    ++++

--- lib/models/orders_model.dart
+++ lib/models/orders_model.dart
@@ -13,34 +13,36 @@ class FoodOrdersModel {
 class FoodOrdersList {
   static List<FoodOrdersModel> getOrderList = [
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 03),
       orderID: "001",
-      orderStatus: "Pending",
+      orderStatus: "Delivered",
     ),
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 03),
       orderID: "002",
-      orderStatus: "Pending",
+      orderStatus: "On the way",
     ),
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 02),
       orderID: "003",
-      orderStatus: "Pending",
+      orderStatus: "Deliverd",
     ),
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 02),
       orderID: "004",
       orderStatus: "Pending",
     ),
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 01),
       orderID: "005",
-      orderStatus: "Pending",
+      orderStatus: "Delivered",
     ),
     FoodOrdersModel(
-      orderDate: DateTime.now(),
+      orderDate: DateTime(2024, 06, 01),
       orderID: "006",
       orderStatus: "Pending",
     ),
   ];
+
+  static List<FoodOrdersModel> displayOrders = List.from(getOrderList);
 }
