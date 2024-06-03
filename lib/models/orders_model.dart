7135eca51966b162bc52948395497b60398a8db0
JohnGastone <johngastone11@gmail.com>
2024-06-03 19:25:53 +0300

    ++++

--- lib/models/orders_model.dart
+++ lib/models/orders_model.dart
@@ -9,3 +9,38 @@ class FoodOrdersModel {
     this.orderStatus,
   });
 }
+
+class FoodOrdersList {
+  static List<FoodOrdersModel> getOrderList = [
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "001",
+      orderStatus: "Pending",
+    ),
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "002",
+      orderStatus: "Pending",
+    ),
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "003",
+      orderStatus: "Pending",
+    ),
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "004",
+      orderStatus: "Pending",
+    ),
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "005",
+      orderStatus: "Pending",
+    ),
+    FoodOrdersModel(
+      orderDate: DateTime.now(),
+      orderID: "006",
+      orderStatus: "Pending",
+    ),
+  ];
+}
