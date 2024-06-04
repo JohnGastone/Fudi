477afa8dc6167e8ef4ea8d148fbd8b66e181b973
JohnGastone <johngastone11@gmail.com>
2024-06-04 13:53:38 +0300

    ++++

--- lib/models/restaurantFoods_model.dart
+++ lib/models/restaurantFoods_model.dart
@@ -1,60 +1,8 @@
-// ignore_for_file: file_names
+import 'package:flutter/material.dart';
 
 class RestaurantfoodsModel {
   String? foodName;
   int? foodPrice;
   int? preparationTime;
-  String? foodImage;
-
-  RestaurantfoodsModel(
-      this.foodName, this.foodPrice, this.foodImage, this.preparationTime);
-}
-
-class RestaurantFoodlist {
-  static List<RestaurantfoodsModel> getRestaurantFood = [
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      20,
-    ),
-    RestaurantfoodsModel(
-      "Kuku nusu",
-      3500,
-      "10-20",
-      25,
-    ),
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      20,
-    ),
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      15,
-    ),
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      15,
-    ),
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      15,
-    ),
-    RestaurantfoodsModel(
-      "Kuku robo",
-      3500,
-      "10-20",
-      15,
-    ),
-  ];
-
-  static List<RestaurantfoodsModel> displayFoods = List.from(getRestaurantFood);
+  Image? foodImage;
 }
