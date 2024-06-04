c68bde240abaa2946f3488abb5431126435532ed
JohnGastone <johngastone11@gmail.com>
2024-06-04 11:06:40 +0300

    ++++

--- lib/models/orders_model.dart
+++ lib/models/orders_model.dart
@@ -21,37 +21,42 @@ class FoodOrdersList {
       orderID: "001",
       orderStatus: "Delivered",
       restaurantName: "Seafood Lovers",
+      orderPrice: 100,
     ),
     FoodOrdersModel(
       orderDate: DateTime(2024, 06, 03),
       orderID: "002",
       orderStatus: "On the way",
       restaurantName: "Shishi Food",
+      orderPrice: 200,
     ),
     FoodOrdersModel(
       orderDate: DateTime(2024, 06, 02),
       orderID: "003",
       orderStatus: "Delivered",
       restaurantName: "Shangazi Cafe",
+      orderPrice: 300,
     ),
     FoodOrdersModel(
       orderDate: DateTime(2024, 06, 02),
       orderID: "004",
       orderStatus: "Pending",
       restaurantName: "Shangazi Cafe",
+      orderPrice: 400,
     ),
     FoodOrdersModel(
       orderDate: DateTime(2024, 06, 01),
       orderID: "005",
       orderStatus: "Delivered",
       restaurantName: "Shangazi Cafe",
+      orderPrice: 500,
     ),
     FoodOrdersModel(
-      orderDate: DateTime(2024, 06, 01),
-      orderID: "006",
-      orderStatus: "Pending",
-      restaurantName: "Shangazi Cafe",
-    ),
+        orderDate: DateTime(2024, 06, 01),
+        orderID: "006",
+        orderStatus: "Pending",
+        restaurantName: "Shangazi Cafe",
+        orderPrice: 350),
   ];
 
   static List<FoodOrdersModel> displayOrders = List.from(getOrderList);
