57629003cad4601b69fd4bd50490aea70ed31aab
JohnGastone <johngastone11@gmail.com>
2024-09-26 11:46:52 +0300

    +++

--- lib/Pages/explorePage.dart
+++ lib/Pages/explorePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fudi/Pages/Authentication/loginPage.dart';
 import 'package:fudi/Pages/Cart/cartPage.dart';
+import 'package:fudi/Pages/Profile/profilePage.dart';
 import 'package:fudi/Pages/foodDescription.dart';
 import 'package:fudi/Pages/restaurantsPage.dart';
 import 'package:fudi/models/categories_model.dart';
@@ -39,180 +40,190 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           automaticallyImplyLeading: false,
           backgroundColor: Color.fromARGB(255, 221, 206, 206),
-          title: Row(
+          title: Column(
             children: [
-              Image.asset(
-                "./assets/fudi.png",
-                height: 60,
-                width: 60,
-              ),
-              SizedBox(
-                width: 35,
-              ),
               Row(
                 children: [
-                  InkWell(
-                    child: Icon(
-                      Icons.location_pin,
-                      size: 20,
-                      color: Colors.green,
-                    ),
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text(
-                                "Location",
-                                style: GoogleFonts.spaceMono(
-                                    fontSize: 16, color: Colors.black),
-                              ),
-                              content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TextField(
-                                    decoration: InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: "Enter your location"),
+                  Image.asset(
+                    "./assets/fudi.png",
+                    height: 60,
+                    width: 60,
+                  ),
+                  SizedBox(
+                    width: 35,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.location_pin,
+                          size: 20,
+                          color: Colors.green,
+                        ),
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(
+                                    "Location",
                                     style: GoogleFonts.spaceMono(
                                         fontSize: 16, color: Colors.black),
                                   ),
-                                  SizedBox(
-                                    height: 10,
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextField(
+                                        decoration: InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            hintText: "Enter your location"),
+                                        style: GoogleFonts.spaceMono(
+                                            fontSize: 16, color: Colors.black),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      SizedBox(
+                                        width: 200,
+                                        child: FloatingActionButton(
+                                            backgroundColor: Colors.green,
+                                            child: Text("Pick from the map",
+                                                style: GoogleFonts.spaceMono(
+                                                    fontSize: 16)),
+                                            onPressed: () {
+                                              // Integration to google maps
+                                            }),
+                                      )
+                                    ],
                                   ),
-                                  SizedBox(
-                                    width: 200,
-                                    child: FloatingActionButton(
-                                        backgroundColor: Colors.green,
-                                        child: Text("Pick from the map",
-                                            style: GoogleFonts.spaceMono(
-                                                fontSize: 16)),
+                                  actions: [
+                                    TextButton(
                                         onPressed: () {
-                                          // Integration to google maps
-                                        }),
-                                  )
-                                ],
-                              ),
-                              actions: [
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text(
-                                      "Cancel",
-                                      style: GoogleFonts.spaceMono(
-                                          fontSize: 16, color: Colors.black),
-                                    )),
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text(
-                                      "OK",
-                                      style: GoogleFonts.spaceMono(
-                                          fontSize: 16, color: Colors.black),
-                                    ))
-                              ],
-                            );
-                          });
-                    },
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                          "Cancel",
+                                          style: GoogleFonts.spaceMono(
+                                              fontSize: 16,
+                                              color: Colors.black),
+                                        )),
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                          "OK",
+                                          style: GoogleFonts.spaceMono(
+                                              fontSize: 16,
+                                              color: Colors.black),
+                                        ))
+                                  ],
+                                );
+                              });
+                        },
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Nathan John",
+                        style: GoogleFonts.spaceMono(fontSize: 16),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.arrow_drop_down,
+                          size: 30,
+                          color: Colors.green,
+                        ),
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(
+                                    "User Manager  ",
+                                    style: GoogleFonts.spaceMono(
+                                        fontSize: 16, color: Colors.black),
+                                  ),
+                                  content: Row(
+                                    children: [
+                                      Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          SizedBox(
+                                            width: 100,
+                                            child: FloatingActionButton(
+                                              backgroundColor: Colors.green,
+                                              child: Text(
+                                                "Log Out",
+                                                style: GoogleFonts.spaceMono(
+                                                    fontSize: 16),
+                                              ),
+                                              onPressed: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            Loginpage()));
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        width: 26,
+                                      ),
+                                      Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          SizedBox(
+                                            width: 100,
+                                            child: FloatingActionButton(
+                                              backgroundColor: Colors.green,
+                                              child: Text(
+                                                "Profile",
+                                                style: GoogleFonts.spaceMono(
+                                                    fontSize: 16),
+                                              ),
+                                              onPressed: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            Profilepage()));
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
+                      )
+                    ],
                   ),
                   SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "Nathan John",
-                    style: GoogleFonts.spaceMono(fontSize: 16),
-                  ),
-                  SizedBox(
-                    width: 10,
+                    width: 30,
                   ),
                   InkWell(
-                    child: Icon(
-                      Icons.arrow_drop_down,
-                      size: 30,
-                      color: Colors.green,
+                    child: CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 221, 206, 206),
+                      child: Icon(
+                        Icons.shopping_cart,
+                        color: Colors.green,
+                      ),
                     ),
                     onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text(
-                                "User Manager  ",
-                                style: GoogleFonts.spaceMono(
-                                    fontSize: 16, color: Colors.black),
-                              ),
-                              content: Row(
-                                children: [
-                                  Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      SizedBox(
-                                        width: 100,
-                                        child: FloatingActionButton(
-                                          backgroundColor: Colors.green,
-                                          child: Text(
-                                            "Log Out",
-                                            style: GoogleFonts.spaceMono(
-                                                fontSize: 16),
-                                          ),
-                                          onPressed: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        Loginpage()));
-                                          },
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    width: 26,
-                                  ),
-                                  Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      SizedBox(
-                                        width: 100,
-                                        child: FloatingActionButton(
-                                          backgroundColor: Colors.green,
-                                          child: Text(
-                                            "Profile",
-                                            style: GoogleFonts.spaceMono(
-                                                fontSize: 16),
-                                          ),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            );
-                          });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => CartPage()));
                     },
-                  )
-                ],
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              InkWell(
-                child: CircleAvatar(
-                  backgroundColor: Color.fromARGB(255, 221, 206, 206),
-                  child: Icon(
-                    Icons.shopping_cart,
-                    color: Colors.green,
                   ),
-                ),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => CartPage()));
-                },
+                ],
               ),
             ],
           ),
